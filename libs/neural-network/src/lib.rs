87ef5126b5b1c08e50df9b208b5dbec0b534c974
Sergei Komarov <komarov.s@cdek.ru>
2024-07-15 16:04:40 +0400

    refactor: Improve random weight generation in neural network

--- libs/neural-network/src/lib.rs
+++ libs/neural-network/src/lib.rs
@@ -1,4 +1,4 @@
-use rand::Rng;
+use rand::{Rng, RngCore};
 
 #[derive(Debug)]
 pub struct LayerTopology {
@@ -17,10 +17,10 @@ impl Network {
             .fold(inputs, |inputs, layer| layer.propagate(inputs))
     }
 
-    pub fn random(layers: &[LayerTopology]) -> Self {
+    pub fn random(rng: &mut dyn RngCore, layers: &[LayerTopology]) -> Self {
         let layers = layers
             .windows(2)
-            .map(|layers| Layer::random(layers[0].neurons, layers[1].neurons))
+            .map(|layers| Layer::random(rng, layers[0].neurons, layers[1].neurons))
             .collect();
 
         Self { layers }
@@ -39,9 +39,9 @@ impl Layer {
             .collect()
     }
 
-    fn random(input_size: usize, output_size: usize) -> Self {
+    fn random(rng: &mut dyn RngCore, input_size: usize, output_size: usize) -> Self {
         let neurons = (0..output_size)
-            .map(|_| Neuron::random(input_size))
+            .map(|_| Neuron::random(rng, input_size))
             .collect();
 
         Self { neurons }
@@ -66,12 +66,53 @@ impl Neuron {
         (self.bias + output).max(0.0)
     }
 
-    fn random(input_size: usize) -> Self {
+    fn random(rng: &mut dyn RngCore, input_size: usize) -> Self {
         let mut rng = rand::thread_rng();
         let bias = rng.gen_range(-1.0..=1.0);
 
         let weights = (0..input_size).map(|_| todo!()).collect();
 
         Self { bias, weights }
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use approx::assert_relative_eq;
+    use rand::SeedableRng;
+    use rand_chacha::ChaCha8Rng;
+
+    #[test]
+    fn random() {
+        let mut rng = ChaCha8Rng::from_seed(Default::default());
+        let neuron = Neuron::random(&mut rng, 4);
+
+        assert_relative_eq!(neuron.bias, -0.6255188);
+
+        assert_relative_eq!(
+            neuron.weights.as_slice(),
+            [0.67383957, 0.8181262, 0.26284897, 0.5238807].as_ref()
+        );
+    }
+
+    #[test]
+    fn propagate() {
+        let neuron = Neuron {
+            bias: 0.5,
+            weights: vec![-0.3, 0.8],
+        };
+
+        // Проверяем работу `.max()` (нашего ReLU):
+        assert_relative_eq!(neuron.propagate(&[-10.0, -10.0]), 0.0,);
+
+        // `0.5` и `1.0` выбраны произвольно:
+        assert_relative_eq!(
+            neuron.propagate(&[0.5, 1.0]),
+            (-0.3 * 0.5) + (0.8 * 1.0) + 0.5,
+        );
+
+        // Мы могли бы сразу написать `1.15`, но полная формула
+        // делает наши намерения более ясными
     }
 }
